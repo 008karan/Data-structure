6ba720e600e75409e29e6a2347ffb58c3be1a4d7
Karan Purohit <008kaaraan@gmail.com>
2017-12-24 20:45:42 +0530

    Update bubble_sort.py

--- bubble_sort.py
+++ bubble_sort.py
@@ -1,11 +1,13 @@
 def bubble_sort(a):
  for i in range(n-1):
-    
+    swap=False
     for j in range(n-1-i):
       if a[j]>a[j+1]:
             
             a[j],a[j+1]=a[j+1],a[j]
-            
+            swap=True
+    if swap==False:
+     break
  return a
 a=input('enter list:')
 a=list(map(int,a.split()))
