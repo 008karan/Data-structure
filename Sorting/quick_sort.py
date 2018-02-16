fb0efd554596c777ab9ea0b5b3aca61e93fe1d02
Karan Purohit <008kaaraan@gmail.com>
2018-02-17 01:03:19 +0530

    random_partition

--- Sorting/quick_sort.py
+++ Sorting/quick_sort.py
@@ -16,7 +16,20 @@ def partition(alist,low,high):
             pivotindex +=1
     alist[pivotindex],alist[high] = alist[high], alist[pivotindex]
     return pivotindex
-   
+  
+#Randomised quick sort
+def random_partition(lst, a ,b):
+    random_index = random.randint(a,b)  # pick random index, its value will be our pivot val
+    lst[b], lst[random_index] = lst[random_index], lst[b]   # swap the value with lst[b]
+
+    pivot = a        # run the rest of the partition code as it was in the original version
+    for i in range(a,b):
+        if lst[i] < lst[b]:
+            lst[i],lst[pivot] = lst[pivot],lst[i]
+            pivot += 1
+    lst[pivot],lst[b] = lst[b],lst[pivot]
+    return pivot
+  
 alist =input('enter list:')
 alist=list(map(int,alist.split()))
 
